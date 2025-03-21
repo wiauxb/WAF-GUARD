754dfb00fe218551b594e6419c0a2872cb4fae9f
wiauxb <44067756+wiauxb@users.noreply.github.com>
2025-03-21 18:00:05 +0100

    updated dump system to work with docker

--- db_dump/dump_db.sh
+++ db_dump/dump_db.sh
@@ -6,8 +6,24 @@ if [ -d $1 ]; then
     echo "Directory already exists. Please provide a new directory."
     exit 1
 fi
-mkdir $1
-sudo chown neo4j:$(whoami) $1
-sudo systemctl stop neo4j
-sudo su neo4j -c "neo4j-admin database dump neo4j --to-path=$1"
-sudo systemctl start neo4j
+
+#check if neo4j container is running
+started_container=false
+if [ -z "$(docker ps -q -f name=neo4j)" ]; then
+    echo "Neo4j container is not running. Starting it now."
+    docker compose up -d --wait neo4j
+    started_container=true
+fi
+
+
+# mkdir $1
+docker compose exec -it neo4j mkdir /tmp/$1
+docker compose exec -it neo4j neo4j stop
+docker compose exec -it neo4j neo4j-admin database dump neo4j --to-path=/tmp/$1
+docker compose cp neo4j:/tmp/$1 .
+docker compose exec -it neo4j neo4j start
+
+if [ $started_container = true ]; then
+    echo "Stopping Neo4j container."
+    docker compose down neo4j
+fi
--- db_dump/restore_dump.sh
+++ db_dump/restore_dump.sh
@@ -1,3 +1,27 @@
-sudo systemctl stop neo4j
-sudo su neo4j -c "neo4j-admin database load neo4j --from-path=$1 --overwrite-destination"
-sudo systemctl start neo4j
+#!/bin/bash
+# Usage: ./restore_dump.sh /path/to/dump
+
+#test if dir already exists
+if [ ! -d $1 ]; then
+    echo "Directory does not exist. Please provide a valid directory."
+    exit 1
+fi
+
+#check if neo4j container is running
+started_container=false
+if [ -z "$(docker ps -q -f name=neo4j)" ]; then
+    echo "Neo4j container is not running. Starting it now."
+    docker compose up -d --wait neo4j
+    started_container=true
+fi
+
+docker compose cp $1 neo4j:/tmp 
+docker compose exec -it neo4j chown -R neo4j:neo4j /tmp/$1
+docker compose exec -it --user neo4j neo4j neo4j stop
+docker compose exec -it --user neo4j neo4j neo4j-admin database load neo4j --from-path=/tmp/$1 --overwrite-destination
+docker compose exec -it --user neo4j neo4j neo4j start
+
+if [ $started_container = true ]; then
+    echo "Stopping Neo4j container."
+    docker compose down neo4j
+fi
