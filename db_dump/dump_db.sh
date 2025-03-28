f2aeb00a1d83d1c39adab7c1107258f4b40d52d0
wiauxb <44067756+wiauxb@users.noreply.github.com>
2025-03-28 20:18:02 +0100

    added postgres to dump system

--- db_dump/dump_db.sh
+++ db_dump/dump_db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Usage: ./dump_db.sh /path/to/dump
 
+source ../.env
+
 #test if dir already exists
 if [ -d $1 ]; then
     echo "Directory already exists. Please provide a new directory."
@@ -15,13 +17,16 @@ if [ -z "$(docker ps -q -f name=neo4j)" ]; then
     started_container=true
 fi
 
-
 # mkdir $1
 docker compose exec -it neo4j mkdir /tmp/$1
 docker compose exec -it neo4j neo4j stop
 docker compose exec -it neo4j neo4j-admin database dump neo4j --to-path=/tmp/$1
 docker compose cp neo4j:/tmp/$1 .
 docker compose exec -it neo4j neo4j start
+
+docker compose exec -it postgres mkdir /tmp/$1
+docker compose exec -it postgres bash -c "pg_dump -U ${POSTGRES_USER} cwaf > /tmp/$1/cwaf.sql"
+docker compose cp postgres:/tmp/$1 .
 
 if [ $started_container = true ]; then
     echo "Stopping Neo4j container."
